b923d0593a0933af3322adcffb24479d4e7106c8
mar <thayshi@yandex.ru>
2017-11-28 23:40:31 +0300

    rewrite scripts on js

--- scripts/script52.js
+++ scripts/script52.js
@@ -1,34 +1,5 @@
-var proverbs=new Array('Я снова пришла в «Один день».', '— Здравствуйте, — улыбается Любовь Гурьевна. — Ну как, вы получили паспорт?', 'Молча кладу паспорт на стол.', '— Вот и замечательно. Ответ на ваш запрос «Как жить» уже сформирован и распечатан, можете ознакомиться с ним.');
-var audio = document.createElement("audio");
-var canPlayMmp3 = !!audio.canPlayType && audio.canPlayType('audio/mp3') != "";
-if(!canPlayMmp3) {
- audio.src = "../music/kabinet.ogg";
-}
-  audio.src = "../music/kabinet.mp3";
-  audio.loop = true;
-  audio.play();
-  function fading() {
-      var timer = setInterval(function() {audio.volume=audio.volume-0.01;
-        if(audio.volume<=0.01){
-          clearInterval(timer);
-        }
-      }, 50);
-    };
-$(document).ready(function(){
-  var i = 0;
-  $('#backSlide').click(function(){
-      i--;
-      $('#slide').text(proverbs[i-1]);
-      console.log(i);
-  });
-  $('#nextSlide').click(function(){
-    if(i<proverbs.length){
-      $('#slide').text(proverbs[i]);
-    }
-    else if(i=proverbs.length){
-      document.getElementById('button-next').style.display='block'; fading();
-    }
-    console.log(i);
-    i++;
-  });
-});
+let proverbs = new Array('Я снова пришла в «Один день».', '— Здравствуйте, — улыбается Любовь Гурьевна. — Ну как, вы получили паспорт?', 'Молча кладу паспорт на стол.', '— Вот и замечательно. Ответ на ваш запрос «Как жить» уже сформирован и распечатан, можете ознакомиться с ним.');
+
+audioContent('kabinet', true);
+
+document.addEventListener('DOMContentLoaded', slider);
--- scripts/script55.js
+++ scripts/script55.js
@@ -1,35 +1,5 @@
-var proverbs=new Array('Я сама буду решать, как мне жить. Прощайте!', 'Я швыряю бумаги и выхожу.');
-var audio = document.createElement("audio");
-var canPlayMmp3 = !!audio.canPlayType && audio.canPlayType('audio/mp3') != "";
-if(!canPlayMmp3) {
- audio.src = "../music/kabinet3.ogg";
-}
-  audio.src = "../music/kabinet3.mp3";
-  audio.loop = true;
-  audio.volume=0.5;
-  audio.play();
-  function fading() {
-      var timer = setInterval(function() {audio.volume=audio.volume-0.01;
-        if(audio.volume<=0.01){
-          clearInterval(timer);
-        }
-      }, 50);
-    };
-$(document).ready(function(){
-  var i = 0;
-  $('#backSlide').click(function(){
-      i--;
-      $('#slide').text(proverbs[i-1]);
-      console.log(i);
-  });
-  $('#nextSlide').click(function(){
-    if(i<proverbs.length){
-      $('#slide').text(proverbs[i]);
-    }
-    else if(i=proverbs.length){
-      document.getElementById('button-next').style.display='block'; fading();
-    }
-    console.log(i);
-    i++;
-  });
-});
+let proverbs = new Array('Я сама буду решать, как мне жить. Прощайте!', 'Я швыряю бумаги и выхожу.');
+
+audioContent('kabinet3', true);
+
+document.addEventListener('DOMContentLoaded', slider);
